3529177c4aa313cdc34bccbd19008da41609166f
Rifat Oktar <105917287+rifatoktar@users.noreply.github.com>
2023-06-17 15:32:34 +0300

    content testing app design with mongodb and express node.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb+srv://test:gAOA39hAEm7QhgmE@flutterexample.li38xcu.mongodb.net/?retryWrites=true&w=majority').on('open', () => {
+const connection = mongoose.createConnection('YOUR_MONGODB_CONNECTION_KEY').on('open', () => {
     console.log("MongoDB Connected");
 }).on('error', () => {
     console.log('MongoDB Connection Error');
